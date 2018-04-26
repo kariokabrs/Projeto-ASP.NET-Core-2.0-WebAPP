07da900b032cf140ab2b2bc3fbae5978673e3a92
Usuario <Usuario@DESKTOP-F2Q933M>
2018-04-26 05:28:48 -0300

    Instanciar o SQLServerLocalDB com os comandos no NuGet  sqllocaldb info ( para checar se tem um SQLLocalDB instalado);         // sqllocaldb create "MyLocalDB" para criar uma instancia do DB chamada MyLocalDB;         // sqllocaldb start "MyLocalDB" para incializar a instancia MyLocalDB;

--- LibraryData/LibraryContext.cs
+++ LibraryData/LibraryContext.cs
@@ -5,6 +5,11 @@ namespace LibraryData
 {
     public class LibraryContext : DbContext
     {
+        // Referenciar a classe Usuario no Projeto AspNetCore.Classes e iniciar o SQL Local DB
+        // Comando no NuGet: 
+        //                  sqllocaldb info ( para checar se tem um SQLLocalDB instalado);
+        //                  sqllocaldb create "MyLocalDB" para criar uma instancia do DB chamada MyLocalDB;
+        //                  sqllocaldb start "MyLocalDB" para incializar a instancia MyLocalDB;
         public LibraryContext(DbContextOptions options) : base(options)
         {
 
