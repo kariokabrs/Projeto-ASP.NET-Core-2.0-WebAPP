d12f62a9d9f068ac1e932ddaf07efba6e1fa5209
Usuario <Usuario@DESKTOP-F2Q933M>
2018-04-30 17:48:17 -0300

    Criada Classe MinhaConfiguracao, que posso chaamr no appsetting.json ou @inect numa pagina chtml, para alterar algumas configurações do sistema sem precisar sair no Source Control assim não se pode ver o source control da página como foram feitas e quais sçao as configurações.

--- AspNetCore/Classes/MinhaConfiguracao.cs
+++ AspNetCore/Classes/MinhaConfiguracao.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace AspNetCore.Classes
 {
+    //Classe para alterar algumas configurações do sistema sem precisar sair no Source Control assim não se pode ver o source control da página como foram feitas e quais sçao as configurações. 
     public class MinhaConfiguracao
     {
         public string MenuBarBgColor { get; set; } = "black";
